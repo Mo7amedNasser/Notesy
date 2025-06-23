cbdba57e46d7b5a82147ee033d2867f6987a6db1
Mohamed Nasser <163488137+Mo7amedNasser@users.noreply.github.com>
2025-06-23 16:27:33 +0300

    Prepared for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import path from "path";
 
 import notesRoutes from "./routes/notesRoutes.js";
 import connectDB from "./config/db.js";
@@ -11,19 +12,31 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 7000;
+const __dirname = path.resolve();
 
 // Middlewares
-app.use(
-  cors({
-    origin: process.env.FRONTEND_URL,
-    methods: ["GET", "POST", "PUT", "DELETE"],
-  })
-);
+if (process.env.NODE_ENV !== "production") {
+  app.use(
+    cors({
+      origin: process.env.FRONTEND_URL,
+      methods: ["GET", "POST", "PUT", "DELETE"],
+    })
+  );
+}
 app.use(express.json());
 app.use(rateLimiter);
 
 // Routes
 app.use("/api/notes", notesRoutes);
+
+// Serve frontend
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("/*splat", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 connectDB().then(() => {
   app.listen(PORT, () => {
