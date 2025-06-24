4d9855589724b53f42c2d438548c59ee1864a302
Mohamed Nasser <163488137+Mo7amedNasser@users.noreply.github.com>
2025-06-24 23:34:43 +0300

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const BASE_URL =
   import.meta.env.MODE === "development"
     ? "http://localhost:5000/api"
-    : import.meta.env.VITE_API_URL + "/api";
+    : import.meta.env.BACKEND_URL + "/api";
 
 const api = axios.create({
   baseURL: BASE_URL,
